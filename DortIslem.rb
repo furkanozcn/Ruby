304913f8ac477f9a549dadbe8e963775c8215a0c
Furkan Özcan <fozcan2@hotmail.com>
2018-08-14 14:23:34 +0300

    test

---
+++ DortIslem.rb
@@ -0,0 +1,22 @@
+class DortIslem
+def toplama(sayi1,sayi2)
+sayi1+sayi2
+end
+toplam=DortIslem.new
+puts toplam.toplama(1,2)
+def cikarma(sayi1,sayi2)
+    sayi1-sayi2
+end
+cikartma=DortIslem.new
+puts cikartma.cikarma(1,3)
+def carpma(sayi1,sayi2)
+    sayi1*sayi2
+end
+carpim=DortIslem.new
+puts carpim.carpma(5,3)
+def bolme(sayi1,sayi2)
+    sayi1/sayi2
+end
+bolum=DortIslem.new
+puts bolum.bolme(5.0,3.0)
+end
