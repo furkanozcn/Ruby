f51ef30cfeaa0e5e434dce585cc82e927c30937c
Furkan Özcan <fozcan2@hotmail.com>
2018-08-15 11:23:32 +0300

    Soru Sayısı Kullanıcı Tarafından Seçilen ve Kullanıcının Başarı Durumunu Gösteren Bir 4 İşlem Testi Olarak Güncellendi.

--- soru_cozumu.rb
+++ soru_cozumu.rb
@@ -1,35 +1,79 @@
 require 'securerandom'
 class Sorular
+    $dsayac=0
+    $ysayac=0
     def sayiOlustur()
-       sayi1=SecureRandom.random_number(100)
-       sayi2=SecureRandom.random_number(100)
-       islem=SecureRandom.random_number(4)
-       @sayi1=sayi1
-       @sayi2=sayi2
-       if(islem==0)
+       @sayi1=SecureRandom.random_number(50)
+       @sayi2=SecureRandom.random_number(50)
+       @islem=SecureRandom.random_number(4)
+       if(@islem==0)
         puts "#{@sayi1}+#{@sayi2}=?"
        end
-       if(islem==1)
+       if(@islem==1)
         puts "#{@sayi1}-#{@sayi2}=?"
        end
-       if(islem==2)
+       if(@islem==2)
         puts "#{@sayi1}*#{@sayi2}=?"
        end
-       if(islem==3)
+       if(@islem==3)
+        if(@sayi2==0)
+            @sayi2=SecureRandom.random_number(49)+1
         puts "#{@sayi1}/#{@sayi2}=?"
+        else
+            puts "#{@sayi1}/#{@sayi2}=?"
+        end
        end   
     end
     def kontrol(sonuc)
-        if (sonuc==(@sayi1+@sayi2)||sonuc==(@sayi1-@sayi2)||sonuc==(@sayi1*@sayi2)||sonuc==(@sayi1/@sayi2))
-            puts "Cevap Doğru"
-        else
-            puts "Cevap Yanlış"
+        if(@islem==0)
+            if (sonuc==(@sayi1+@sayi2))
+                $dsayac+=1
+                puts "Cevap Doğru"
+            else
+                $ysayac+=1
+                puts "Cevap Yanlış"  
+            end
+        end
+        if(@islem==1)
+            if (sonuc==(@sayi1-@sayi2))
+                $dsayac+=1
+                puts "Cevap Doğru"
+            else
+                $ysayac+=1
+                puts "Cevap Yanlış"  
+            end
+        end
+        if(@islem==2)
+            if (sonuc==(@sayi1*@sayi2))
+                $dsayac+=1
+                puts "Cevap Doğru"
+            else
+                $ysayac+=1
+                puts "Cevap Yanlış"  
+            end
+        end
+        if(@islem==3)
+            if (sonuc==(@sayi1/@sayi2))
+                $dsayac+=1
+                puts "Cevap Doğru"
+            else
+                $ysayac+=1
+                puts "Cevap Yanlış"  
+            end
         end
     end
 end
+puts "Kaç Soru Çözmek İstersiniz?"
+adet=gets.chop
+$i = 0
 sonuclar=Sorular.new
-puts sonuclar.sayiOlustur()
-puts "Sonuç:"
-sonuc=gets.chop
-puts sonuclar.kontrol(sonuc.to_i)
-
+begin  
+    puts "Soru #{$i+1}:"
+    puts sonuclar.sayiOlustur()
+    puts "Sonuç:"
+    sonuc=gets.chop
+    puts sonuclar.kontrol(sonuc.to_i)
+    $i+=1 
+end until $i==adet.to_i
+puts "#{$i} soruda #{$dsayac.to_i} doğru, #{$ysayac.to_i} yanlış var"
+puts "Soruların %#{$i*100*($dsayac/100.to_f)} doğru cevapladınız."
